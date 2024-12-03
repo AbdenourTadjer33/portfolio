e6ce334c479b302c71b01898a5d0473e97484462
Abdennour Tadjer <abdennour.tadjer@gmail.com>
2024-12-03 04:07:50 +0100

    constant file added

---
+++ src/data/constant.ts
@@ -0,0 +1,9 @@
+const constants = {
+    emailAddress: "abdennour.tadjer@gmail.com",
+    linkedinUrl: "https://www.linkedin.com/in/tadjer-abdenour-549a22247/",
+    githubUrl: "https://github.com/abdenourtadjer33",
+
+
+}
+
+export default constants;
